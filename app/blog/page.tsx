fb0690ae5cb23ec7d8a2875282fa6aa8473126e4
wogara <weogara@gmail.com>
2024-08-15 22:57:58 -0400

    updating colors

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -11,15 +11,15 @@ export default async function Blog() {
 
   return (
     <main className="container mx-auto p-6 py-24">
-      <h1 className="text-4xl font-bold text-center mb-12">Blog</h1>
+      <h1 className="text-4xl font-bold text-text text-center mb-8">Blog</h1>
       <ul className="space-y-8">
         {allBlogsData.map(({ slug, date, title, tags }) => (
-          <li key={slug} className="p-6 bg-white rounded-lg shadow-md">
+          <li key={slug} className="p-6 bg-peach rounded-lg shadow-md">
             <Link href={`/blog/${slug}`}>
                 {title}
             </Link>
-            <p className="text-gray-500 mt-2">{new Date(date).toLocaleDateString()}</p>
-            <p className="text-gray-500 mt-1">
+            <p className="text-text mt-2">{new Date(date).toLocaleDateString()}</p>
+            <p className="text-text mt-1">
               <span className="font-semibold">Tags:</span> {tags.join(", ")}
             </p>
           </li>
